c4abe8ea64bafaf1a45023f6f4f42ebe043a7d42
Arjun Munji <arjunmunji@gmail.com>
2016-03-06 06:23:21 +0530

    new routes and stuff

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var Place     = require('./app/models/place');
 var latitude;
 var longitude;
 var url;
-var user_key;
+var user_key = 'e1fd503923ef41dd65f0772663565809';
 var info;
 
 // ROUTES FOR OUR API
@@ -54,7 +54,7 @@ router.route('/geocode')
     latitude = req.headers.lat;
     longitude = req.headers.lng;
     url = 'https://developers.zomato.com/api/v2.1/geocode?lat='+latitude+'&lon='+longitude;
-    user_key = 'e1fd503923ef41dd65f0772663565809';
+
 
     var options = {
         url: url,
@@ -63,62 +63,82 @@ router.route('/geocode')
         }
     };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    info = JSON.parse(body);
+		function callback(error, response, body) {
+  		if (!error && response.statusCode == 200) {
+				info = JSON.parse(body);
 
-    var restaurantCode = info.nearby_restaurants[1].restaurant.R.res_id;
-    console.log(restaurantCode);
+		    // var restaurantCode = info.nearby_restaurants[1].restaurant.R.res_id;
+		    // console.log(restaurantCode);
 
-		var restaurantCodes = [];
-		var restaurantLat = [];
-		var restaurantLng = [];
-		var userRatingVotes = [];
+				var restaurantCodes = [];
+				var restaurantLat = [];
+				var restaurantLng = [];
+				var userRatingVotes = [];
+				var restaurantNames = [];
 
-		for (var restaurant in info.nearby_restaurants) {
-			if (info.nearby_restaurants.hasOwnProperty(restaurant)) {
+				for (var restaurant in info.nearby_restaurants) {
+					if (info.nearby_restaurants.hasOwnProperty(restaurant)) {
 
+					}
+				}
+				// console.log(restaurant);
+
+				for (var i = 0; i < restaurant; i ++) {
+					restaurantCodes[i] = info.nearby_restaurants[i+1].restaurant.R.res_id;
+					restaurantLat[i] = info.nearby_restaurants[i+1].restaurant.location.latitude;
+					restaurantLng[i] = info.nearby_restaurants[i+1].restaurant.location.longitude;
+					aggregateRatings[i] = info.nearby_restaurants[i+1].restaurant.user_rating.aggregate_rating;
+					restaurantNames[i] = info.nearby_restaurants[i+1].restaurant.name;
+				}
+
+
+				var responseJSON = JSON.stringify({names: restaurantNames, codes: restaurantCodes, lats: restaurantLat, lngs: restaurantLng, ratings: aggregateRatings});
+
+				res.setHeader('Content-Type', 'application/json');
+				res.send(responseJSON);
+		  }
+		  else {
+	      res.json(error);
 			}
 		}
-		console.log(restaurant);
 
-		for (var i = 0; i < restaurant; i++) {
-			restaurantCodes[i] = info.nearby_restaurants[i+1].restaurant.R.res_id;
-			restaurantLat[i] = (info.nearby_restaurants[i+1].restaurant.location.latitude);
-			restaurantLng[i] = (info.nearby_restaurants[i+1].restaurant.location.longitude);
-			userRatingVotes[i] = (info.nearby_restaurants[i+1].restaurant.user_rating.votes);
-		}
-
-
-		var responseJSON = JSON.stringify({codes: restaurantCodes, lats: restaurantLat, lngs: restaurantLng, votes: userRatingVotes});
-
-		res.setHeader('Content-Type', 'application/json');
-		res.send(responseJSON);
-  }
-  else {
-      res.json(error);
-		}
-}
-
-
-request(options, callback);
+	request(options, callback);
     // res.json({message: 'Geocode is '+latitude+' latitude & '+longitude+' longitude & url='+url + info});
-
 })
 
 router.route('/test')
 
 .get(function(req, res) {
 	res.send("Hi there. Your connection is 100% OK");
-});
+})
 
 
-
-
-// .sendGET(function (url, user_key) {
-
-// })
-
+router.route('/restaurant/:res_id')
+.get(function(req, res) {
+	var res_id = req.params.res_id;
+	url = "https://developers.zomato.com/api/v2.1/restaurant?res_id="+res_id;
+	var options = {
+			url: url,
+			headers: {
+					'user_key': user_key
+			}
+	};
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			info = JSON.parse(body);
+			res.setHeader('Content-Type', 'application/json');
+			var imageURL = info.featured_image;
+			var avgCostForTwo = info.average_cost_for_two;
+			var address = info.location.address;
+			var responseJSON = JSON.stringify({address: address, image_url: imageURL, cost_for_two: avgCostForTwo});
+			res.send(responseJSON);
+		}
+		else {
+			res.json(error);
+		}
+	}
+request(options, callback);
+})
 	// get all the places (accessed at GET http://localhost:8080/api/places)
 
 
